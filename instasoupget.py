21e7c3eb649a0bf88512608c817902644bdf78e7
Thomas Lauritzen <tommer.rl@live.dk>
2017-07-04 18:26:39 +0200

    Edited to get url from RUN
    Now it gets the url from RUN (windows key + R)

--- instasoupget.py
+++ instasoupget.py
@@ -1,24 +1,18 @@
+#! python3.5
 import bs4 as bs
 import requests, webbrowser
 import sys, os
 
-curpath = os.getcwd()
+uInput = sys.argv[1] #instagram link please
 
+sauce = requests.get(uInput) #python looks up the link
+print(uInput) #python tells you which link it looks at
+#sauce.raise_for_status()
+soup = bs.BeautifulSoup(sauce.text, "lxml") #making the sauce into soup
 
-sauce = requests.get("<HERE - instagram post url - HERE>")
-sauce.raise_for_status()
-soup = bs.BeautifulSoup(sauce.text, "lxml")
-
-#find = soup.select("meta")
-#print(find)
-#for meta in find:
- #   if meta.endswith("og:image\"/>"):
-  #      print(meta)
-
-find = soup.find("meta", property="og:image")
-print(find)
-#title = soup.find("meta",  property="og:title")
-findstr = str(find)
+find = soup.find("meta", property="og:image") #python looks for this the source code
+print(find) #python is proud that it found it and tells you so you can be proud with it
+findstr = str(find) 
 slice1 = findstr[15:]
 imageurl = slice1[:-23]
 filename = imageurl[57:]
@@ -27,10 +21,10 @@ print("filename = " + filename)
 
 instaimg = requests.get(imageurl)
 instaimg.status_code
-#len(instaimg.text)
+os.chdir("e:\\test") #image gets saved here
+curpath = os.getcwd()
 file = open(filename, "wb")
 for chunk in instaimg.iter_content(100000):
     file.write(chunk)
 file.close()
 print("File should be saved to: " + curpath)
-#webbrowser.open(imageurl)
