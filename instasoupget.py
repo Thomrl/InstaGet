1ff2d1e271dcf220373b9a0234791b0af3c49248
Thomas Lauritzen <tommer.rl@live.dk>
2017-07-09 20:54:59 +0200

    deleted junk and added some comments

--- instasoupget.py
+++ instasoupget.py
@@ -1,28 +1,25 @@
 #! python3.5
 import bs4 as bs
-import requests, webbrowser
-import sys, os
+import requests, sys, os
 
 uInput = sys.argv[1] #instagram link please
 
 sauce = requests.get(uInput) #python looks up the link
-print(uInput) #python tells you which link it looks at
+print("Given url: " +uInput) #python tells you which link it looks at
 #sauce.raise_for_status()
 soup = bs.BeautifulSoup(sauce.text, "lxml") #making the sauce into soup
 
-find = soup.find("meta", property="og:image") #python looks for this the source code
-print(find) #python is proud that it found it and tells you so you can be proud with it
+find = soup.find("meta", property="og:image") #python looks for this in the source code
 findstr = str(find) 
-slice1 = findstr[15:]
-imageurl = slice1[:-23]
+imageurl = findstr[15:-23]
 filename = imageurl[57:]
-print("imageurl is = " + imageurl)
+print("imageurl = " + imageurl)
 print("filename = " + filename)
 
 instaimg = requests.get(imageurl)
-instaimg.status_code
-os.chdir("e:\\test") #image gets saved here
-curpath = os.getcwd()
+#instaimg.status_code
+os.chdir("E:\\instagram") #Where you want i saved
+curpath = os.getcwd() #passing the directory to a variable so python can print it later
 file = open(filename, "wb")
 for chunk in instaimg.iter_content(100000):
     file.write(chunk)
