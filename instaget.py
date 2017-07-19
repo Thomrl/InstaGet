7944ebe8389361bf443b83538918c455d31eefc0
Thomas Lauritzen <tommer.rl@live.dk>
2017-07-19 13:58:15 +0200

    infoandget() function improved
    infoandget() function improved, and getthis() function has been removed.

--- instaget.py
+++ instaget.py
@@ -6,9 +6,6 @@ if len(sys.argv) > 1: #If windows RUN gets more words than 1 it assumes that the
 else:                 #If the script just gets started without additional input it will take what's in the clipboard (Ctrl+c)
     uInput = pyperclip.paste()
 
-os.chdir("E:\\instagram") #Chosen directory
-curpath = os.getcwd()     #puts the dir into a variable
-
 sauce = requests.get(uInput)                #Python gets the webpage
 soup = bs.BeautifulSoup(sauce.text, "lxml") #making the sauce into soup
 
@@ -22,7 +19,6 @@ filename = str(filename)[2:-2]
 IGimageUrl = re.findall(r"display_url\S+\s\S+", str(soup))
 IGimageUrl = re.findall(r"http\S+\jpg", str(IGimageUrl))
 videoUrl = re.findall(r"http\S+.mp4", str(soup))
-youtube = re.findall(r"youtube", str(soup))
 YTimageURL = re.findall(r"https://i.ytimg.com\S+.jpg", str(soup))
 twitchUrl = re.findall(r"http.+?[^\"]*", str(videoUrl))
 twitchTitle = re.findall(r"channel_title:.+\"", str(soup))
@@ -32,28 +28,25 @@ twitchTitle = str(twitchTitle)[3:-3]
 def splitter(detected):
     print(detected+"\n-------------------------------------------------------------------\n")
 
-def getthis(ftype):
-    file = open(saveas, "wb")
-    for chunk in (ftype).iter_content(100000):
-        file.write(chunk)
-    file.close()
-    print("File = " + saveas + " saved to: " + curpath) #Succes!
-
 def infoandget(typeUrl, ftype, fext): #typeUrl e.g imageUrl - filetype e.g image - fileextension e.g .jpg
-    saveas = username + "_" + filename + fext
+    if filename == username: #If it's a youtube or twitch link
+        saveas = filename + fext
+        os.chdir("notInstagram")  #Chosen directory
+    else:
+        saveas = username + "_" + filename + fext
+        os.chdir("E:\\instagram") #Chosen directory
+    curpath = os.getcwd()         #puts the dir into a variable
     file = open(saveas, "wb")
     for chunk in (ftype).iter_content(100000):
         file.write(chunk)
     file.close()
     print("Filename = " + filename + fext + "\nFound url = " + typeUrl + "\nFile = " + saveas +" saved to: " + curpath) #All the info
 #
-if len(youtube) > 10:
+if len(re.findall(r"youtube", str(soup))) > 10:
     #YOUTUBE IS SPECIAL
     splitter("Youtube thumbnail") #---------------------------------------------------------------------
     imageUrl = YTimageURL[0]
     image = requests.get(imageUrl)
-    os.chdir("notInstagram")
-    curpath = os.getcwd()
     filename = username
     infoandget(imageUrl, image, ".jpg")
 elif len(re.findall(r"twitch", str(soup))) > 10:
@@ -61,21 +54,19 @@ elif len(re.findall(r"twitch", str(soup))) > 10:
     splitter("Twitch clip") #---------------------------------------------------------------------
     videoUrl = twitchUrl[0]
     video = requests.get(videoUrl)
-    os.chdir("notInstagram")
-    curpath = os.getcwd()
-    print("Filename = " + str(twitchTitle) + "\nFound url = " + videoUrl)
-    saveas = str(twitchTitle) + ".mp4"
-    getthis(video)
+    filename = str(twitchTitle).split("|", 1)[0] #Lots of clip titles seem to have "|" and windows cant use that for filenaming
+    username = filename
+    infoandget(videoUrl, video, ".mp4")
 elif len(IGimageUrl) > 1:
     #INSTAGRAM GALLERY
     print("Instagram gallery " + "- Images found = " +str(len(IGimageUrl)-1))
     for i in range(1, len(IGimageUrl)):
         splitter("") #-----------------------------------------------------------------
-        ImageUrl = IGimageUrl
-        image = requests.get(ImageUrl[i])
-        print("Filename = " + filename + str(i) + ".jpg \nImageurl = " + ImageUrl[i])
-        saveas = username + "_" + filename + str(i) + ".jpg"
-        getthis(image) # open > DL > Close function
+        imageUrl = IGimageUrl
+        image = requests.get(imageUrl[i])
+        filename = filename + str(i)
+        imageUrl = imageUrl[i]
+        infoandget(imageUrl, image, ".jpg")
 elif len(videoUrl) > 1:
     #INSTAGRAM VIDEO
     splitter("Instagram video") #---------------------------------------------------------------------
